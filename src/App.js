b9e617b2a7d3bbb096115f3c0c6870ff4582a740
Federico Giovannini <giovannini.federico@hotmail.it>
2021-03-05 21:13:06 +0100

    getCountries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    // The code inside here will run once when the component loads, not again
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, // united States, United Kingdom, France
+            value: country.countryInfo.iso2, // US, UK, FR
+          }));
+
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
-      <h1>Let's build a Covid19 tracker</h1>
+      <div className="app_header">
+        <h1>Covid19 tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through all the countries and show them as options */}
+
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        {/* Header */}
+        {/* Title + select input dropdown field */}
+        {/* InfoBoxs */}
+        {/* InfoBoxs */}
+        {/* InfoBoxs */}
+        {/* Table */}
+        {/* Graph */}
+        {/* Map */}
+      </div>
     </div>
   );
 }
